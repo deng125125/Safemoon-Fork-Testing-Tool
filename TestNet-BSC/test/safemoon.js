937c3550f120252eaad6cb151b9e46af408e898b
deng125125 <55897181+deng125125@users.noreply.github.com>
2021-12-21 20:37:29 -0500

    Add single transfer, multiple transfer, buy tokens

--- TestNet-BSC/test/safemoon.js
+++ TestNet-BSC/test/safemoon.js
@@ -10,14 +10,11 @@ const web3 = new Web3(rpcURL);
 contract('Safemoon', (accounts) => {
   let SafemoonInstance;
   let PancakeRouterInstance;
-  // let WBNBInstance;
-  // let pairInstance;
   const deployer = accounts[0];
   const PancakeRouterAddress = "0xD99D1c33F9fC3444f8101754aBC46c52416550D1";
   let SafemoonAddress;
   let PairAddress;
-  // const WBNBAddress;
-  // let pairAddress; // Safemoon-wbnb pair
+  let WBNBAddress;
 
   const numToAddliquidityinBNB = 500; // min number to add liquidity in BNB/ETH (update it to match the contract)
   const numToAddliquidity = web3.utils.toWei(numToAddliquidityinBNB.toString(), 'ether'); 
@@ -31,12 +28,81 @@ contract('Safemoon', (accounts) => {
 
     SafemoonAddress = SafemoonInstance.options.address;
     PairAddress = await SafemoonInstance.methods.uniswapV2Pair().call();
+    WBNBAddress = await PancakeRouterInstance.methods.WETH().call();
 
     // addLiquidityEth
-    const numToAddLiquidityEth =  toWei((10 * numToAddliquidityinBNB).toString()); 
+    // const numToAddLiquidityEth =  toWei((10 * numToAddliquidityinBNB).toString()); 
+    const numToAddLiquidityEth =  toWei(numToAddliquidityinBNB.toString());
     await SafemoonInstance.methods.approve(PancakeRouterAddress, toWei('100000000000000')).send({from: deployer}); // approve
     await PancakeRouterInstance.methods.addLiquidityETH(SafemoonAddress, numToAddLiquidityEth, 0, 0, SafemoonAddress, 2639271011)
       .send({ from: deployer, value: toWei('500'), gas: 1200000000 }); // add LiquidityETH 500 BNB / 5000 Safemoon
+  });
+
+  it('single transaction succeed', async () => {
+    let taxFee = await SafemoonInstance.methods._taxFee.call().call();
+    let liquidityFee = await SafemoonInstance.methods._liquidityFee.call().call();
+    await SafemoonInstance.methods.transfer(accounts[2], toWei('100'))
+      .send({from: deployer, gas: 1200000000});
+    await SafemoonInstance.methods.transfer(accounts[4], toWei('100'))
+      .send({from: deployer, gas: 1200000000});
+    //balance before transfer
+    const senderContractSafemoonBalance0 = await balanceOf(SafemoonInstance, accounts[2]);
+    const receiverContractSafemoonBalance0 = await balanceOf(SafemoonInstance, accounts[3]);
+    const holderContractSafemoonBalance0 = await balanceOf(SafemoonInstance, accounts[4]); 
+    const contractSafemoonBalance0 = await balanceOf(SafemoonInstance, SafemoonAddress);
+
+    // trigger auto addLiquidity
+    await SafemoonInstance.methods.transfer(accounts[3], toWei('100'))
+      .send({from: accounts[2], gas: 1200000000});
+    
+    // balances after auto addLiquidity
+    const senderContractSafemoonBalance1 = await balanceOf(SafemoonInstance, accounts[2]);
+    const receiverContractSafemoonBalance1 = await balanceOf(SafemoonInstance, accounts[3]);
+    const holderContractSafemoonBalance1 = await balanceOf(SafemoonInstance, accounts[4]); 
+    const contractSafemoonBalance1 = await balanceOf(SafemoonInstance, SafemoonAddress);
+
+    assert.equal(senderContractSafemoonBalance1, 0);//taxes are correctly deducted from sender
+    assert.ok(fromWei((receiverContractSafemoonBalance1 - receiverContractSafemoonBalance0).toString()) > (100 - taxFee - liquidityFee).toString());//receiver 
+    assert.ok(fromWei((contractSafemoonBalance1 - contractSafemoonBalance0).toString()) > taxFee.toString());//take liqudity fee
+    assert.ok(holderContractSafemoonBalance0 < holderContractSafemoonBalance1);//check reflect
+  });
+
+  it('Token transfer succeed for 100 times', async () => {
+    let taxFee = await SafemoonInstance.methods._taxFee.call().call();
+    let liquidityFee = await SafemoonInstance.methods._liquidityFee.call().call();
+    await SafemoonInstance.methods.transfer(accounts[2], toWei('100'))
+      .send({from: deployer, gas: 1200000000});
+    await SafemoonInstance.methods.transfer(accounts[4], toWei('100'))
+      .send({from: deployer, gas: 1200000000});
+
+    let senderContractSafemoonBalance0;
+    let receiverContractSafemoonBalance0;
+    let holderContractSafemoonBalance0; 
+    let contractSafemoonBalance0;
+    let senderContractSafemoonBalance1;
+    let receiverContractSafemoonBalance1;
+    let holderContractSafemoonBalance1; 
+    let contractSafemoonBalance1;
+
+    for (let i = 0; i < 100; i++) {
+      senderContractSafemoonBalance0 = await balanceOf(SafemoonInstance, accounts[2]);
+      receiverContractSafemoonBalance0 = await balanceOf(SafemoonInstance, accounts[3]);
+      holderContractSafemoonBalance0 = await balanceOf(SafemoonInstance, accounts[4]); 
+      contractSafemoonBalance0 = await balanceOf(SafemoonInstance, SafemoonAddress); 
+
+      await SafemoonInstance.methods.transfer(accounts[3], toWei('1'))
+        .send({from: accounts[2], gas: 1200000000}); 
+
+      senderContractSafemoonBalance1 = await balanceOf(SafemoonInstance, accounts[2]);
+      receiverContractSafemoonBalance1 = await balanceOf(SafemoonInstance, accounts[3]);
+      holderContractSafemoonBalance1 = await balanceOf(SafemoonInstance, accounts[4]); 
+      contractSafemoonBalance1 = await balanceOf(SafemoonInstance, SafemoonAddress);
+
+      //assert.equal(fromWei((senderContractSafemoonBalance0 - senderContractSafemoonBalance1).toString()), `1`);
+      assert.ok(fromWei(((receiverContractSafemoonBalance1 - receiverContractSafemoonBalance0) * 100).toString()) > (100 - taxFee - liquidityFee).toString());
+      assert.ok(fromWei(((contractSafemoonBalance1 - contractSafemoonBalance0) * 100).toString()) > taxFee.toString());
+      assert.ok(holderContractSafemoonBalance0 < holderContractSafemoonBalance1);
+    }
   });
 
   it('addliquidity() can be triggered once', async () => {
@@ -90,7 +156,6 @@ contract('Safemoon', (accounts) => {
     console.log("withdrawable BNB in contract after addLiquidity(): ", hasWithdrawableBNB);
   });
 
-
   it('Contract can receive BNB', async () => {
     const sender = accounts[1];
     const senderBNB0 = await getBalanceBNB(sender);
@@ -102,9 +167,44 @@ contract('Safemoon', (accounts) => {
     assert.equal(fromWei((contractBNB1 - contractBNB0).toString()), '10');
   });
 
+  // for burnfee, only test if any token is burned
+  it('Buy tokens from Router', async () => {
+    const buyer = accounts[1];
+    const otherOne = accounts[2];
 
+    // transfer tokens to buyer
+    await SafemoonInstance.methods.transfer(otherOne, numToAddliquidity)
+      .send({from: deployer, gas: 1200000000});
+    const otherOneBalance0 = await balanceOf(SafemoonInstance, otherOne);
+
+    const BuyTokenNumberinBNB = numToAddliquidityinBNB / 10; // buy Safemoon Tokens
+    let path = new Array(WBNBAddress, SafemoonAddress);
+    assert.equal(await balanceOf(SafemoonInstance, SafemoonAddress), '0', 'initial safemoon contract balance is not 0');
+    await PancakeRouterInstance.methods.swapETHForExactTokens(toWei(BuyTokenNumberinBNB.toString()), path, buyer, 2639271011)
+      .send({from: buyer, value: toWei('1000'), gas: 12000000});
+
+    //buy feeRates to update
+    const liquidityFeeRate = 5;
+    const marketingFeeRate = 0;
+    const burnFeeRate = 5;
+
+    const liquidityFeeinFinney = BuyTokenNumberinBNB * 1000 * liquidityFeeRate / 100;
+    const marketingFeeinFinney = BuyTokenNumberinBNB * 1000 * marketingFeeRate / 100;
+    const burnFeeinBNB = BuyTokenNumberinBNB * burnFeeRate / 100;
+
+     // burnFee
+    const otherOneBalance1 = await balanceOf(SafemoonInstance, otherOne);
+    assert.ok(otherOneBalance1 > otherOneBalance0, "no burn fee!");
+
+    // test addLiquidityFee
+    assert.equal(Math.floor(fromWeiToFinney(await balanceOf(SafemoonInstance, SafemoonAddress))),
+                        liquidityFeeinFinney + marketingFeeinFinney, 
+                        'liquidity fees and marketing fees do not go to contract');
+  });
 
 });
+
+
 
 const getBalanceBNB = (address) => {
   return web3.eth.getBalance(address);
@@ -120,4 +220,8 @@ const toWei = (numString) => {
 
 const fromWei = (numString) => {
   return web3.utils.fromWei(numString, 'ether');
+}
+
+const fromWeiToFinney = (numString) => {
+  return web3.utils.fromWei(numString, 'finney');
 }
