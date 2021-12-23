96897d61bfea5427884c91c1318da99747fe1e00
deng125125 <55897181+deng125125@users.noreply.github.com>
2021-12-23 14:47:07 -0500

    Fix bugs

--- mainnet.sh
+++ mainnet.sh
@@ -1,17 +1,46 @@
 #!/bin/sh
 
-chmod +x bashes/mainnet/installer.sh
-chmod +x bashes/mainnet/runGanache-cli.sh
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "already has brew"
+fi
 
-#install packages
-bashes/mainnet/installer.sh
+# Add Homebrew to your PATH in /Users/$USER/.zprofile:
+echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/$USER/.zprofile
+eval $(/opt/homebrew/bin/brew shellenv)
 
-# open a new Terminal tab
-osascript -e 'tell application "Terminal" to do script "'"'$(PWD)'"'/bashes/mainnet/runGanache-cli.sh"'
-sleep 7
+which -s node
+if [[ $? != 0 ]] ; then
+    # Install node
+    brew install node
+else
+    echo "already has node"
+fi
 
-cd Mainnet-BSC
+#no npm fund
+npm config set fund false --global
 
-truffle compile
+which -s truffle
+if [[ $? != 0 ]] ; then
+    # Install truffle
+    brew install truffle
+else
+    echo "already has truffle"
+fi
 
-truffle test test/safemoon.js
+which -s truffle
+if [[ $? != 0 ]] ; then
+    # Install truffle
+    npm install -g truffle
+else
+    echo "already has truffle"
+fi
+
+npm install web3
+
+npm install -g -D ganache-cli
+
+echo "installation complete"
