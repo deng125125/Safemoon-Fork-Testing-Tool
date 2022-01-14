3785438df5517a1a9b93993273d92d12b8e1cf96
deng125125 <55897181+deng125125@users.noreply.github.com>
2022-01-14 12:24:04 -0500

    Create mainnet_not_install.sh

---
+++ mainnet_not_install.sh
@@ -0,0 +1,27 @@
+
+#!/bin/sh
+opt=$1
+
+# chmod +x bashes/mainnet/installer.sh
+chmod +x bashes/mainnet/runGanache-cli.sh
+
+#install packages
+# bashes/mainnet/installer.sh
+
+# open a new Terminal tab
+osascript -e 'tell application "Terminal" to do script "'"'$(PWD)'"'/bashes/mainnet/runGanache-cli.sh"'
+sleep 7
+
+cd Mainnet-BSC
+
+truffle compile
+
+case $opt in
+        -r|-R )
+		truffle test test/reflect.js
+		;;
+	* )
+		truffle test test/safemoon.js
+		;;
+esac
+#truffle test test/safemoon.js
