e1922e4fc1d7ad1539abf299ed8753f11c0b4bf9
deng125125 <55897181+deng125125@users.noreply.github.com>
2022-09-13 11:57:10 -0400

    Update runGanache-cli.sh
    ganache-cli doesn't support --allowUnlimitedContractSize anymore

--- bashes/mainnet/runGanache-cli.sh
+++ bashes/mainnet/runGanache-cli.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-npx ganache-cli -f https://bsc-dataseed.binance.org --defaultBalanceEther 10000 --allowUnlimitedContractSize --gasLimit 10000000000
+npx ganache-cli -f https://bsc-dataseed.binance.org --defaultBalanceEther 10000 --gasLimit 10000000000
